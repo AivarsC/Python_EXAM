b56125064c337db819df23db27c3e0c8cb81a672
AivarsC <aivaryz@gmail.com>
2022-09-28 21:38:59 +0300

    added solution to Uzduotis_1 main.py

--- Uzduotis_1/main.py
+++ Uzduotis_1/main.py
@@ -24,3 +24,46 @@ users = [
     { 'id': '8', 'name': 'Simon Peterson', 'age': 32, 'hasDog': False, 'hasCat': True },
     { 'id': '9', 'name': 'Daniel Cane', 'age': 15, 'hasDog': False, 'hasCat': False },
 ]
+
+# =================  Sprendimas  ==================
+
+import pprint 
+
+# 1 funkcija
+
+def filter_all_or_nothing_people(people):
+    """Grąžina vartotojus, kurie arba neturi naminių gyvūnų visiškai, arba turi ir šunį, ir katę."""
+
+    res = []
+    for person in people:
+        if person['hasDog'] == False and person['hasCat'] == False or person['hasDog'] == True and person['hasCat'] == True:
+            res.append(person)
+
+    return res
+
+# 2 funkcija
+
+def filter_underaged_owners(owners):
+    """Grąžina vartotojus, kurie dar nėra pilnamečiai, bet jau turi bent vieną naminį gyvūną."""
+
+    res = []
+    for owner in owners:
+        if owner['age'] <= 18 and (owner['hasDog'] == True or owner['hasCat'] == True):
+            res.append(owner)
+
+    return res
+
+# ===================================
+# Testing:
+# ===================================
+
+pp = pprint.PrettyPrinter(indent=5)
+
+print("# ------------Test 1 funkcija:------------")
+all_or_nothing_people = filter_all_or_nothing_people(users)
+pp.pprint(all_or_nothing_people)
+
+print("# ------------Test 2 funkcija:------------")
+teens = filter_underaged_owners(users)
+pp.pprint(teens)
+
