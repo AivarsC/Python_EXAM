1494ce9926c01ffd8705a0e57e816e2785d58e4c
AivarsC <aivaryz@gmail.com>
2022-09-29 20:33:06 +0300

    add imports to Uzduotis_5 main.py

--- Uzduotis_5/main.py
+++ Uzduotis_5/main.py
@@ -1,5 +1,11 @@
 # Importuokite reikiamus modulius, kad veiktų žemiau nurodytos funkcijos:
 
+import modules.num.ints as integers
+from modules.mathematics.addition import add as add
+from modules.mathematics.division import divide as divide
+from modules.mathematics.multiplication import multiply as multiply
+from modules.mathematics.subtraction import subtract as subtract
+
 # Kitų failų ir žemiau esančio kodo nekeiskite
 a = add(integers.one, integers.four)
 b = divide(integers.four, integers.two)
