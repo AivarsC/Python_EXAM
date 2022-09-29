34a9681f8416a6b86c6176d576c3989e21e8f290
AivarsC <aivaryz@gmail.com>
2022-09-29 19:43:17 +0300

    add solution to Uzduotis_4 main

--- Uzduotis_4/main.py
+++ Uzduotis_4/main.py
@@ -15,3 +15,37 @@
 # Metodas:
 # * was_expensive() - jeigu filmo "budget" yra daugiau nei 100 mln. USD,
 #   grąžins True, kitu atveju - False.
+
+
+# =================  Sprendimas  ==================
+
+class Movie:
+    """Describes movie with properties: title, director and budget"""
+    def __init__(self, title, director, budget):
+        self.title = title
+        self.director = director
+        self.budget = budget
+
+    def __str__(self):
+        """ Returns info about movie"""
+        return (f'Movie title: "{self.title}", Director: {self.director}, Budget: {self.budget} USD')
+
+    def was_expensive(self):
+        """" Return TRUE when budget is greater than 100M USD, and FAlSE when less than 100M """
+        if self.budget > 100000000:
+            return True
+        else:
+            return False
+
+movie_1 = Movie('Pirates of the Caribbean: On Stranger Tides', 'Rob Marshall', 422000000)
+movie_2 = Movie('Redirected', 'Emilis Velyvis', 2700000)
+
+# ===================================
+# Testing:
+# ===================================
+
+print(movie_1)
+print('Was the movie expensive? ', movie_1.was_expensive())
+
+print(movie_2)
+print('Was the movie expensive? ', movie_2.was_expensive())
